c68f91726b0d91d0bddddd018f7f6d3d7577651f
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-08-20 19:37:34 +0900

    /にアクセスした場合に/participantsにリダイレクトするようにnext.config.mjsに追記

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,15 @@ const nextConfig = {
             },
         ],
     },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/participants',
+                permanent: true,
+            },
+        ]
+    },
 };
 
 export default nextConfig;
