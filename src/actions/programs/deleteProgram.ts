68a8ec67fc144200bc33856b02716e8cf0c68096
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-08-20 19:32:32 +0900

    企画の削除は現状行わないのでdeleteProgramsをコメント化

--- src/actions/programs/deleteProgram.ts
+++ src/actions/programs/deleteProgram.ts
@@ -1,26 +1,24 @@
-// パス: /actions/programs/deleteProgram.ts
-import { supabase } from '@/lib/supabaseClient';
-import { revalidatePath } from 'next/cache';
+// // パス: /actions/programs/deleteProgram.ts
+// import { supabase } from '@/lib/supabaseClient';
+// import { revalidatePath } from 'next/cache';
 
-interface Response {
-  success: boolean;
-  error?: any;
-}
+// interface Response {
+//   success: boolean;
+//   error?: any;
+// }
 
-export default async function deleteProgram(
-  id: string
-): Promise<Response> {
-  const { error } = await supabase
-    .from('programs')
-    .delete()
-    .eq('id', id);
+// export default async function deleteProgram(
+//   id: string,
+//   target: Target
+// ): Promise<Response> {
+//   const { error } = await supabase.from('programs').delete().eq('id', id);
 
-  if (error) {
-    console.error('Error deleting Program:', error);
-    return { success: false, error };
-  }
+//   if (error) {
+//     console.error('Error deleting Program:', error);
+//     return { success: false, error };
+//   }
 
-  revalidatePath('/');
+//   revalidatePath('/');
 
-  return { success: true };
-}
+//   return { success: true };
+// }
